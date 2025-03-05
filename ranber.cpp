63c2cdcd9bc635ea5c8ef43549eda16214609636
lumor <2784019182@qq.com>
2025-03-05 22:44:24 +0800

    一个生成随机数的函数

--- ranber.cpp
+++ ranber.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include<random>
+
 using namespace std;
+
+//写出一个随机生成1到5的函数
+int ran_ber()
+{    
+    std::random_device rd;  // 随机数种子
+    std::mt19937 gen(rd());  // 随机数生成器
+
+    std::uniform_int_distribution<> dis(1, 5);  // 定义随机数范围
+
+    return dis(gen);  // 生成随机数并返回
+}
+//调用ran_ber检查是否输出随机数
 int main()
 {
-    cout<<"hello,suduko!"<<endl;
+    int rand=0;
+    for(int i=0;i<10;i++)
+    {
+        rand=ran_ber();
+        cout<<rand<<endl;
+    }
 
     return 0;
 }
